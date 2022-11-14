dadc7a95a953679410b76d2f6e4f57a8d91a004e
bruno <brunofellip.silva@gmail.com>
2022-11-14 15:02:54 -0300

    regex in xml of energy bill

--- analises/leitor.py
+++ analises/leitor.py
@@ -1,13 +1,30 @@
-import pandas as pd
-from PyPDF2 import PdfReader
 import re
 
-reader = PdfReader(r"D:\visualcode\data_analistyc\files\Enel.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
+file = r"D:\visualcode\data_analistyc\files\0.xml"
 
-values = re.findall(r'\d+,\d+', text)
-faturamento = re.findall(r'^([a-z].*?[\d+,\d+])$', text, flags=re.I)
-print(values)
-# print(faturamento)
+with open(file, "r") as f:
+    xml = f.read()
+    
+# print(xml)
+xml_splited = xml.split('</line>')
+# print(xml_splited)
+list_escrever = []
+for x in xml_splited:
+    lines = re.findall(r'c=".*/>\n*', x)
+    # print(lines)
+    line = rf'{lines}'
+    # print(line.replace('c="','').replace('"/>', ''))
+    line_t1 = line.replace('c="','').replace('"/>', '')
+    line_t2 = line_t1.replace(r'\n','').replace("'", '')
+    line_true = line_t2.replace(r', ','').replace("[",'').replace("]",'')
+    # print(line_true)
+    list_escrever.append(line_true)
+    
+print(list_escrever)
+
+with open('Arquivo final.txt', "w") as arquivo:
+    for linha in list_escrever:
+        arquivo.write(f"{linha}\n")
+    arquivo.write("Arquivo escrito por Bruno Fellip para a empresa 4Docs")  
+arquivo.close()
+
